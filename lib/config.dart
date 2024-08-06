02d3b2c0bf3dd425eeaf05be29980fe90722792b
Paul Agada <paulagada075@gmail.com>
2024-08-06 17:10:37 +0100

    first

---
+++ lib/config.dart
@@ -0,0 +1,6 @@
+final url = 'http://192.168.49.141:3000/';
+final registration = url + "registration";
+final login = url + 'login';
+final addtodo = url + 'createTodo';
+final getToDoList = url + 'getUserTodo';
+final deleteTodo = url + 'deleteTodo';
